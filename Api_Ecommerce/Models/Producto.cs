e676d3131e843f542b20b468983a6b12a65f56ff
trinidadbenegas <ruben@DESKTOP-VQ6EFRG>
2023-10-12 11:08:55 -0300

    adding models

---
+++ Api_Ecommerce/Models/Producto.cs
@@ -0,0 +1,19 @@
+﻿namespace Api_Ecommerce.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public double Precio { get; set; }
+
+        
+
+
+    }
+}
